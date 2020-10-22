4d0b3fa6e1dbb7110954bc2506ac33216dd6f0a6
junjypark <73309648+junjypark@users.noreply.github.com>
2020-10-22 19:20:05 -0400

    Update SpLoc.R

--- R/SpLoc.R
+++ R/SpLoc.R
@@ -1,9 +1,9 @@
-SpLoc=function(NN, ymat, nperm=5000, alpha=0.05, seed=NULL){
-  if (length(which(is(NN)=="sparseMatrix"))==0){
+SpLoc=function(NNmatrix, ymat, nperm=5000, alpha=0.05, seed=NULL){
+  if (length(which(is(NNmatrix)=="sparseMatrix"))==0){
     stop("NN is not a sparse matrix. Please refer the Matrix R package to convert it")
   }
 
-  if ( ncol(NN)!=nrow(ymat) ){
+  if ( ncol(NNmatrix)!=nrow(ymat) ){
     stop("The number of columns of NN and the number of rows of ymat needs to be the same (# voxels)")
   }
 
@@ -13,33 +13,33 @@ SpLoc=function(NN, ymat, nperm=5000, alpha=0.05, seed=NULL){
     set.seed(seed)
   }
 
-  out=SpLocC(NN, ymat, nperm, alpha)
+  out=SpLocC(NNmatrix, ymat, nperm, alpha)
   out$pvalue=(1+sum(c(out$permMax)>max(out$Tstat)))/(1+nperm)
 
   return(out)
 }
 
-ClusterSearch=function(tstat, thres, NNmat){
-  if (length(tstat)!=nrow(NNmat)){
+ClusterSearch=function(tstat, thres, NNmatrix){
+  if (length(tstat)!=nrow(NNmatrix)){
     stop("The number of rows in NNmat needs to be the same as the length of tstat,")
   }
 
   sig.ind=which(tstat>thres)
   tstat=tstat[sig.ind]
-  NNmat=NNmat[sig.ind,,drop=F]
+  NNmatrix=NNmatrix[sig.ind,,drop=F]
 
-  if (nrow(NNmat)==0){bool=F}else{bool=T}
+  if (nrow(NNmatrix)==0){bool=F}else{bool=T}
 
   clust=1
   sig=NULL
   while(bool){
     ind=which(tstat==max(tstat))[1]
-    sig.vertices=which(NNmat[ind,]!=0)
+    sig.vertices=which(NNmatrix[ind,]!=0)
     sig=c(sig, sig.vertices)
-    out.set=which(apply(NNmat[,sig.vertices,drop=F],1,max)>0)
+    out.set=which(apply(NNmatrix[,sig.vertices,drop=F],1,max)>0)
 
     tstat=tstat[-out.set]
-    NNmat=NNmat[-out.set,,drop=F]
+    NNmatrix=NNmatrix[-out.set,,drop=F]
 
     print(paste0("Cluster ", clust, " with ", length(sig.vertices), " voxel(s) is selected (total=", length(sig), ")" ))
     clust=clust+1
