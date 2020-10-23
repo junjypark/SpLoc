ee4949ce94f6c5e9c63c992b3c9f9c1809e7436d
junjypark <73309648+junjypark@users.noreply.github.com>
2020-10-23 13:52:10 -0400

    Update SpLoc.R

--- R/SpLoc.R
+++ R/SpLoc.R
@@ -19,34 +19,3 @@ SpLoc=function(NNmatrix, ymat, nperm=5000, alpha=0.05, seed=NULL){
   return(out)
 }
 
-ClusterSearch=function(tstat, thres, NNmatrix){
-  if (length(tstat)!=nrow(NNmatrix)){
-    stop("The number of rows in NNmat needs to be the same as the length of tstat.")
-  }
-
-  sig.ind=which(tstat>thres)
-  tstat=tstat[sig.ind]
-  NNmatrix=NNmatrix[sig.ind,,drop=F]
-
-  if (nrow(NNmatrix)==0){bool=F}else{bool=T}
-
-  clust=1
-  sig=NULL
-  while(bool){
-    ind=which(tstat==max(tstat))[1]
-    sig.vertices=which(NNmatrix[ind,]!=0)
-    sig=c(sig, sig.vertices)
-    out.set=which(apply(NNmatrix[,sig.vertices,drop=F],1,max)>0)
-
-    tstat=tstat[-out.set]
-    NNmatrix=NNmatrix[-out.set,,drop=F]
-
-    print(paste0("Cluster ", clust, " with ", length(sig.vertices), " voxel(s) is selected (total=", length(sig), ")." ))
-    clust=clust+1
-
-    if (length(tstat)==0){bool=F}
-  }
-
-  return(sig)
-}
-
