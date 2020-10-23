d16230d4d71f9b78c349e38fbcf7cb6cc943b882
junjypark <73309648+junjypark@users.noreply.github.com>
2020-10-23 14:29:48 -0400

    Update SpLoc.R

--- R/SpLoc.R
+++ R/SpLoc.R
@@ -2,9 +2,11 @@ SpLoc=function(NNmatrix, ymat, nperm=5000, alpha=0.05, seed=NULL){
   if (length(which(is(NNmatrix)=="sparseMatrix"))==0){
     stop("NN is not a sparse matrix. Please refer the Matrix R package to convert it.")
   }
-
   if ( ncol(NNmatrix)!=nrow(ymat) ){
     stop("The number of columns of NN and the number of rows of ymat needs to be the same (# voxels).")
+  }
+  if ( alpha<0 |alpha>1){
+    stop("alpha should range between 0 and 1.")
   }
 
   if (is.null(seed)){
