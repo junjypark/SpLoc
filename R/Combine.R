3f06b9fb8bc27f06234ebb651e697ca2329a21bf
junjypark <73309648+junjypark@users.noreply.github.com>
2020-10-24 14:33:30 -0400

    Create Combine.R

---
+++ R/Combine.R
@@ -0,0 +1,25 @@
+combine=function(lst, alpha=0.05){
+  n=length(lst)
+  seed=do.call("c",lapply(lst, function(x){x$seed}))
+  nperm=do.call("c",lapply(lst, function(x){x$nperm}))
+  
+  if (length(unique(seed))>1){stop("Use the same seed for every Sploc output.")}
+  if (length(unique(nperm))>1){stop("Use the same number of permutations for every SpLoc output.")}
+  
+  n.cand=do.call("c",lapply(lst, function(x){length(x$Tstat)}))
+  
+  Tstat=do.call("c",lapply(lst, function(x){x$Tstat}))
+  permMax=apply(do.call("cbind",lapply(lst, function(x){x$permMax})),1,max)
+  threshold=quantile(permMax, 1-alpha)
+  pvalue=(1+sum(c(permMax)>max(Tstat)))/(1+nperm[1])
+  index=which(Tstat>threshold)
+  
+  return(list(
+    threshold=threshold,
+    Tstat=Tstat,
+    permMax=permMax,
+    pvalue=pvalue,
+    seed=seed[1],
+    nperm=nperm[1]
+  ))
+}
