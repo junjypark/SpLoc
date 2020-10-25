69d055397d4ab3e1a4a5da543f0b054d97b5a1d9
junjypark <73309648+junjypark@users.noreply.github.com>
2020-10-25 01:06:53 -0400

    Update getResidMatrix.R

--- R/getResidMatrix.R
+++ R/getResidMatrix.R
@@ -1,6 +1,6 @@
 getResidMatrix=function(ymat, X=NULL, mask){
   mask.index=which(mask!=0)
-  if (X=NULL){
+  if (X==NULL){
     for (j in mask.index){
       ymat[,j]=ymat[,j]/sum(ymat[,j]^2,na.rm=T)
     }
