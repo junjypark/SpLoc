4813d4bacaf3f80a5e254f19483d40ca82502300
junjypark <73309648+junjypark@users.noreply.github.com>
2020-10-25 13:22:57 -0400

    Update SpLoc.R

--- R/SpLoc.R
+++ R/SpLoc.R
@@ -1,35 +1,4 @@
-SpLoc=function(NNmatrix, ymat, nperm=5000, alpha=0.05, seed=NULL, is.sparse=F){
-  if (length(which(is(NNmatrix)=="sparseMatrix"))==0){
-    stop("NN is not a sparse matrix. Please refer the Matrix R package to convert it.")
-  }
-  if ( ncol(NNmatrix)!=nrow(ymat) ){
-    stop("The number of columns of NN and the number of rows of ymat needs to be the same (# voxels).")
-  }
-  if ( alpha<0 |alpha>1){
-    stop("alpha should range between 0 and 1.")
-  }
-  if (is.null(seed)){
-    stop("Specifying a seed value is required.")
-  }
-  if (is.sparse){
-    index=NULL
-    for (j in 1:ncol(NNmatrix)){
-      if(sum(NNmatrix[,j])!=0){ index=c(index,j)}
-    }
-    NNmatrix=NNmatrix[,index]
-    ymat=ymat[index,]
-  }
-
-  pU=big.matrix(nrow(NNmatrix), nperm, type = "double")
-  out=SpLocC(NNmatrix, ymat, nperm, alpha, seed, pU@address)
-  out$pvalue=(1+sum(c(out$permMax)>max(out$Tstat,na.rm=TRUE)))/(1+nperm)
-  out$seed=seed
-  out$nperm=nperm
-  return(out)
-}
-
-
-SpLoc2=function(NNmatrix, ymat, nperm=1000, alpha=0.05, seed=NULL, 
+SpLoc=function(NNmatrix, ymat, nperm=1000, alpha=0.05, seed=NULL, 
                 is.sparse=F,partition=F, npartition=NULL, parallel=F, ncores=1){
   if (length(which(is(NNmatrix)=="sparseMatrix"))==0){
     stop("NN is not a sparse matrix. Please refer the Matrix R package to convert it.")
