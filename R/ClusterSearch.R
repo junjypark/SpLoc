786771e026b830a2f5279917f3a027679211f255
junjypark <73309648+junjypark@users.noreply.github.com>
2020-10-29 12:55:08 -0400

    Update ClusterSearch.R

--- R/ClusterSearch.R
+++ R/ClusterSearch.R
@@ -1,10 +1,10 @@
-ClusterSearch=function(tstat, thres, NNmatrix){
-  if (length(tstat)!=nrow(NNmatrix)){
-    stop("The number of rows in NNmat needs to be the same as the length of tstat.")
+ClusterSearch=function(Tstat, threshold, NNmatrix){
+  if (length(Tstat)!=nrow(NNmatrix)){
+    stop("The number of rows in NNmat needs to be the same as the length of Tstat.")
   }
   
-  sig.ind=which(tstat>thres)
-  tstat=tstat[sig.ind]
+  sig.ind=which(Tstat>threshold)
+  Tstat=Tstat[sig.ind]
   NNmatrix=NNmatrix[sig.ind,,drop=F]
   
   if (nrow(NNmatrix)==0){bool=F}else{bool=T}
@@ -12,18 +12,18 @@ ClusterSearch=function(tstat, thres, NNmatrix){
   clust=1
   sig=NULL
   while(bool){
-    ind=which(tstat==max(tstat))[1]
+    ind=which(Tstat==max(Tstat))[1]
     sig.vertices=which(NNmatrix[ind,]!=0)
     sig=c(sig, sig.vertices)
     out.set=which(apply(NNmatrix[,sig.vertices,drop=F],1,max)>0)
     
-    tstat=tstat[-out.set]
+    Tstat=Tstat[-out.set]
     NNmatrix=NNmatrix[-out.set,,drop=F]
     
     print(paste0("Cluster ", clust, " with ", length(sig.vertices), " voxel(s) is selected (total=", length(sig), ")." ))
     clust=clust+1
     
-    if (length(tstat)==0){bool=F}
+    if (length(Tstat)==0){bool=F}
   }
   
   return(sig)
