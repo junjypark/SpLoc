4f44063574b15b6dd6aa67d6d23b19962721b768
junjypark <73309648+junjypark@users.noreply.github.com>
2020-11-09 14:20:09 -0500

    Update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -28,22 +28,6 @@ BEGIN_RCPP
     return R_NilValue;
 END_RCPP
 }
-// SpLocC2
-Rcpp::List SpLocC2(arma::sp_mat& NNmatrix, arma::mat& ymat, int nperm, double alpha, int s, SEXP pU);
-RcppExport SEXP _SpLoc_SpLocC2(SEXP NNmatrixSEXP, SEXP ymatSEXP, SEXP npermSEXP, SEXP alphaSEXP, SEXP sSEXP, SEXP pUSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< arma::sp_mat& >::type NNmatrix(NNmatrixSEXP);
-    Rcpp::traits::input_parameter< arma::mat& >::type ymat(ymatSEXP);
-    Rcpp::traits::input_parameter< int >::type nperm(npermSEXP);
-    Rcpp::traits::input_parameter< double >::type alpha(alphaSEXP);
-    Rcpp::traits::input_parameter< int >::type s(sSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type pU(pUSEXP);
-    rcpp_result_gen = Rcpp::wrap(SpLocC2(NNmatrix, ymat, nperm, alpha, s, pU));
-    return rcpp_result_gen;
-END_RCPP
-}
 // SpLocC
 Rcpp::List SpLocC(arma::sp_mat& NNmatrix, arma::mat& ymat, int nperm, double alpha, int s, SEXP pU, SEXP pY);
 RcppExport SEXP _SpLoc_SpLocC(SEXP NNmatrixSEXP, SEXP ymatSEXP, SEXP npermSEXP, SEXP alphaSEXP, SEXP sSEXP, SEXP pUSEXP, SEXP pYSEXP) {
@@ -58,23 +42,6 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< SEXP >::type pU(pUSEXP);
     Rcpp::traits::input_parameter< SEXP >::type pY(pYSEXP);
     rcpp_result_gen = Rcpp::wrap(SpLocC(NNmatrix, ymat, nperm, alpha, s, pU, pY));
-    return rcpp_result_gen;
-END_RCPP
-}
-// SpLocDiffC2
-Rcpp::List SpLocDiffC2(arma::sp_mat& NNmatrix, arma::mat& ymat, arma::vec group, int nperm, double alpha, int s, SEXP pU);
-RcppExport SEXP _SpLoc_SpLocDiffC2(SEXP NNmatrixSEXP, SEXP ymatSEXP, SEXP groupSEXP, SEXP npermSEXP, SEXP alphaSEXP, SEXP sSEXP, SEXP pUSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< arma::sp_mat& >::type NNmatrix(NNmatrixSEXP);
-    Rcpp::traits::input_parameter< arma::mat& >::type ymat(ymatSEXP);
-    Rcpp::traits::input_parameter< arma::vec >::type group(groupSEXP);
-    Rcpp::traits::input_parameter< int >::type nperm(npermSEXP);
-    Rcpp::traits::input_parameter< double >::type alpha(alphaSEXP);
-    Rcpp::traits::input_parameter< int >::type s(sSEXP);
-    Rcpp::traits::input_parameter< SEXP >::type pU(pUSEXP);
-    rcpp_result_gen = Rcpp::wrap(SpLocDiffC2(NNmatrix, ymat, group, nperm, alpha, s, pU));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -100,9 +67,7 @@ END_RCPP
 static const R_CallMethodDef CallEntries[] = {
     {"_SpLoc_quantileC", (DL_FUNC) &_SpLoc_quantileC, 2},
     {"_SpLoc_set_seed", (DL_FUNC) &_SpLoc_set_seed, 1},
-    {"_SpLoc_SpLocC2", (DL_FUNC) &_SpLoc_SpLocC2, 6},
     {"_SpLoc_SpLocC", (DL_FUNC) &_SpLoc_SpLocC, 7},
-    {"_SpLoc_SpLocDiffC2", (DL_FUNC) &_SpLoc_SpLocDiffC2, 7},
     {"_SpLoc_SpLocDiffC", (DL_FUNC) &_SpLoc_SpLocDiffC, 8},
     {NULL, NULL, 0}
 };
