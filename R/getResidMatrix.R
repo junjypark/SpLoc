4272211fc1eb72a0319aeab75eddc46e47f0e3cb
junjypark <73309648+junjypark@users.noreply.github.com>
2020-11-08 23:03:59 -0500

    Update getResidMatrix.R

--- R/getResidMatrix.R
+++ R/getResidMatrix.R
@@ -4,7 +4,14 @@ getResidMatrix=function(ymat, X=NULL, mask){
     for (j in mask.index){
       ymat[,j]=ymat[,j]/sum(ymat[,j]^2,na.rm=T)
     }
+  } else{
+    X=as.matrix(X)
+    n=nrow(X); p=ncol(X)
+    P=tcrossprod(tcrossprod(X,solve(crossprod(X))),X)
+    Q=1-apply(P,1,sum)
+    ymat=Q*ymat
   }
   ymat[is.nan(ymat)]=0
   return(ymat)
 }
+
