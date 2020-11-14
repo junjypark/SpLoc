09258accc1261d8876937fbadbe6af0a142679df
junjypark <73309648+junjypark@users.noreply.github.com>
2020-11-14 14:37:58 -0500

    Update SpLoc_support.cpp

--- src/SpLoc_support.cpp
+++ src/SpLoc_support.cpp
@@ -52,7 +52,7 @@ void set_seed(unsigned int seed) {
 
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
-Rcpp::List SpLocMeanC(arma::sp_mat& NNmatrix, arma::mat& ymat, int nperm, double alpha, int s){
+Rcpp::List SpLocMeanC(arma::mat& ymat, arma::sp_mat& NNmatrix, int nperm, double alpha, int s){
   int q=NNmatrix.n_rows;
   int n=ymat.n_cols;
   arma::vec onevec(n); onevec.fill(1);
@@ -94,7 +94,7 @@ Rcpp::List SpLocMeanC(arma::sp_mat& NNmatrix, arma::mat& ymat, int nperm, double
 
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
-Rcpp::List SpLocDiffC(arma::sp_mat& NNmatrix, arma::mat& ymat, arma::vec group, int nperm, double alpha, int s){
+Rcpp::List SpLocDiffC(arma::mat& ymat, arma::sp_mat& NNmatrix, arma::vec group, int nperm, double alpha, int s){
   int q=NNmatrix.n_rows;
   int n=group.size();
   arma::vec U(q);
@@ -133,91 +133,3 @@ Rcpp::List SpLocDiffC(arma::sp_mat& NNmatrix, arma::mat& ymat, arma::vec group, 
                             Rcpp::Named("nperm")=nperm);
 }
 
-
-
-// // [[Rcpp::depends(RcppArmadillo)]]
-// // [[Rcpp::export]]
-// Rcpp::List SpLocMeanC2(arma::sp_mat& NNmatrix, arma::mat& ymat, int nperm, double alpha, int s, SEXP pU){
-//   int q=NNmatrix.n_rows;
-//   int n=ymat.n_cols;
-//   arma::vec onevec(n); onevec.fill(1);
-//   arma::vec U(q);
-//   double sd;
-//   
-//   XPtr<BigMatrix> xpMat(pU);
-//   arma::mat permU = arma::Mat<double> ( (double *)xpMat->matrix(), xpMat->nrow(), xpMat->ncol(), false);
-//   
-//   U=NNmatrix*ymat*onevec;  
-// 
-//   arma::mat flipmat(n,nperm); 
-//   set_seed(s);
-//   flipmat.randn(); 
-//   flipmat=sign(flipmat);
-//   permU=NNmatrix*ymat*flipmat;
-//   
-//   for (int k=0; k<q; ++k){
-//     sd=stddev(permU.row(k));
-//     permU.row(k)=permU.row(k)/sd;
-//     U(k)=U(k)/sd;
-//   }
-//   
-//   permU=permU%permU;
-//   U=U%U;  
-//   
-//   arma::vec permMax(nperm);
-//   for (int i=0; i<nperm; ++i){
-//     permMax(i)=permU.col(i).max();
-//   }
-//   
-//   double qt=quantileC(permMax, alpha);  
-//   
-//   return Rcpp::List::create(Rcpp::Named("threshold")=qt,
-//                             Rcpp::Named("Tstat")=U,
-//                             Rcpp::Named("permMax")=permMax,
-//                             Rcpp::Named("nperm")=nperm);
-// }
-// 
-// 
-// // [[Rcpp::depends(RcppArmadillo)]]
-// // [[Rcpp::export]]
-// Rcpp::List SpLocDiffC2(arma::sp_mat& NNmatrix, arma::mat& ymat, arma::vec group, int nperm, double alpha, int s, SEXP pU){
-//   int q=NNmatrix.n_rows;
-//   int n=group.size();
-//   arma::vec U(q);
-//   double sd;
-// 
-//   XPtr<BigMatrix> xpMat(pU);
-//   arma::mat permU = arma::Mat<double> ( (double *)xpMat->matrix(), xpMat->nrow(), xpMat->ncol(), false);
-// 
-//   U=NNmatrix*ymat*group;  
-//   
-//   arma::mat permmat(n,nperm);
-//   set_seed(s);
-//   for (int i=0; i<nperm; ++i){
-//     permmat.col(i)=shuffle(group);
-//   }
-// 
-//   permU=NNmatrix*ymat*permmat;
-//   
-//   for (int k=0; k<q; ++k){
-//     sd=stddev(permU.row(k));
-//     permU.row(k)=permU.row(k)/sd;
-//     U(k)=U(k)/sd;
-//   }
-// 
-//   permU=permU%permU;
-//   U=U%U;
-// 
-//   arma::vec permMax(nperm);
-//   for (int i=0; i<nperm; ++i){
-//     permMax(i)=permU.col(i).max();
-//   }
-//   
-//   double qt=quantileC(permMax, alpha);
-//   
-//   return Rcpp::List::create(Rcpp::Named("threshold")=qt,
-//                             Rcpp::Named("Tstat")=U,
-//                             Rcpp::Named("permMax")=permMax,
-//                             Rcpp::Named("nperm")=nperm);
-// }
-// 
