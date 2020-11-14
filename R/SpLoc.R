4dd88a6b57899eaa0981dae9641da4c5701448c1
junjypark <73309648+junjypark@users.noreply.github.com>
2020-11-14 14:37:54 -0500

    Update SpLoc.R

--- R/SpLoc.R
+++ R/SpLoc.R
@@ -1,9 +1,15 @@
-SpLoc=function(NNmatrix, ymat, group=NULL, nperm=1000, alpha=0.05, seed=NULL, 
+SpLoc=function(ymat, NNmatrix=NULL, group=NULL, nperm=1000, alpha=0.05, seed=NULL, 
                is.sparse=F,partition=F, npartition=1, parallel=F, ncores=1){
+  if (is.null(NNmatrix)){
+    print("As NNmatrix is not specified, SpLoc conducts massive univariate analysis.")
+    NNmatrix=Matrix(diag(nrow(ymat)),sparse=T)
+  }
+  
   if (is.null(group)){
     return(
-      SpLocMean(NNmatrix=NNmatrix, 
-                ymat=ymat, nperm=nperm, 
+      SpLocMean(ymat=ymat, 
+                NNmatrix=NNmatrix, 
+                nperm=nperm, 
                 alpha=alpha, 
                 seed=seed,
                 is.sparse = is.sparse, 
@@ -15,8 +21,8 @@ SpLoc=function(NNmatrix, ymat, group=NULL, nperm=1000, alpha=0.05, seed=NULL,
   }
   else{
     return(
-      SpLocDiff(NNmatrix=NNmatrix, 
-                ymat=ymat, 
+      SpLocDiff(ymat=ymat, 
+                NNmatrix=NNmatrix, 
                 group=group, 
                 nperm=nperm, 
                 alpha=alpha, 
@@ -31,7 +37,7 @@ SpLoc=function(NNmatrix, ymat, group=NULL, nperm=1000, alpha=0.05, seed=NULL,
 }
 
 
-SpLocMean=function(NNmatrix, ymat, nperm=1000, alpha=0.05, seed=NULL, 
+SpLocMean=function(ymat, NNmatrix, nperm=1000, alpha=0.05, seed=NULL, 
                    is.sparse=F,partition=F, npartition=1, parallel=F, ncores=1){
   if (length(which(is(NNmatrix)=="sparseMatrix"))==0){
     stop("NN is not a sparse matrix. Please refer the Matrix R package to convert it.")
@@ -77,13 +83,13 @@ SpLocMean=function(NNmatrix, ymat, nperm=1000, alpha=0.05, seed=NULL,
       cl=makeCluster(ncores)
       registerDoParallel(cl)
       result=foreach(i=1:npartition, .packages=("SpLoc"),.noexport = "SpLocC" )%dopar%{
-        SpLocMeanC(NNList[[i]], ymat, nperm, alpha, seed)
+        SpLocMeanC(ymat, NNList[[i]], nperm, alpha, seed)
       }
       stopCluster(cl)
     } else{
       result=list()
       for (i in 1:npartition){
-        result[[i]]=SpLocMeanC(NNList[[i]], ymat, nperm, alpha, seed)
+        result[[i]]=SpLocMeanC(ymat, NNList[[i]], nperm, alpha, seed)
       }
     }
     
@@ -91,7 +97,7 @@ SpLocMean=function(NNmatrix, ymat, nperm=1000, alpha=0.05, seed=NULL,
     return(out)
     
   } else{
-    out=SpLocMeanC(NNmatrix, ymat, nperm, alpha, seed)
+    out=SpLocMeanC(ymat, NNmatrix, nperm, alpha, seed)
     out$pvalue=(1+sum(c(out$permMax)>max(out$Tstat,na.rm=TRUE)))/(1+nperm)
     out$seed=seed
     return(out)    
@@ -99,7 +105,7 @@ SpLocMean=function(NNmatrix, ymat, nperm=1000, alpha=0.05, seed=NULL,
 }
 
 
-SpLocDiff=function(NNmatrix, ymat, group, nperm=1000, alpha=0.05, seed=NULL, 
+SpLocDiff=function(ymat, NNmatrix, group, nperm=1000, alpha=0.05, seed=NULL, 
                    is.sparse=F,partition=F, npartition=1, parallel=F, ncores=1){
   if (!all.equal(sort(unique(group)),c(-1,1))){
     stop("group should have either 1 or -1.")
@@ -152,13 +158,13 @@ SpLocDiff=function(NNmatrix, ymat, group, nperm=1000, alpha=0.05, seed=NULL,
       cl=makeCluster(ncores)
       registerDoParallel(cl)
       result=foreach(i=1:npartition, .packages=("SpLoc"),.noexport = "SpLocC" )%dopar%{
-        SpLocDiffC(NNList[[i]], ymat, group, nperm, alpha, seed)
+        SpLocDiffC(ymat, NNList[[i]], group, nperm, alpha, seed)
       }
       stopCluster(cl)
     } else{
       result=list()
       for (i in 1:npartition){
-        result[[i]]=SpLocDiffC(NNList[[i]], ymat, group, nperm, alpha, seed)
+        result[[i]]=SpLocDiffC(ymat, NNList[[i]], group, nperm, alpha, seed)
       }
     }
     
@@ -166,160 +172,10 @@ SpLocDiff=function(NNmatrix, ymat, group, nperm=1000, alpha=0.05, seed=NULL,
     return(out)
     
   } else{
-    out=SpLocDiffC(NNmatrix, ymat, group, nperm, alpha, seed)
+    out=SpLocDiffC(ymat, NNmatrix, group, nperm, alpha, seed)
     out$pvalue=(1+sum(c(out$permMax)>max(out$Tstat,na.rm=TRUE)))/(1+nperm)
     out$seed=seed
     return(out)    
   }
 }
 
-
-
-# SpLocMean2=function(NNmatrix, ymat, nperm=1000, alpha=0.05, seed=NULL, 
-#                    is.sparse=F,partition=F, npartition=1, parallel=F, ncores=1){
-#   if (length(which(is(NNmatrix)=="sparseMatrix"))==0){
-#     stop("NN is not a sparse matrix. Please refer the Matrix R package to convert it.")
-#   }
-#   if ( ncol(NNmatrix)!=nrow(ymat) ){
-#     stop("The number of columns of NN and the number of rows of ymat needs to be the same (# voxels).")
-#   }
-#   if ( alpha<0 |alpha>1){
-#     stop("alpha should range between 0 and 1.")
-#   }
-#   if (is.null(seed)){
-#     stop("Specifying a seed value is required.")
-#   }
-#   if (is.sparse){
-#     index=NULL
-#     for (j in 1:ncol(NNmatrix)){
-#       if(sum(NNmatrix[,j])!=0){ index=c(index,j)}
-#     }
-#     NNmatrix=NNmatrix[,index]
-#     ymat=ymat[index,]
-#   }
-#   if (isTRUE(partition)){
-#     if (is.null(npartition)){
-#       partition=FALSE
-#       print("partition set to be FALSE as npartition is not specified.")
-#     }
-#     else if (npartition==1){
-#       partition=FALSE
-#       print("partition set to be FALSE as npartition is 1.")
-#     }
-#   }
-#   
-#   if (isTRUE(partition)){
-#     NNList=list()
-#     len=ceiling(nrow(NNmatrix)/npartition)
-#     for (i in 1:npartition){
-#       start=len*(i-1)+1
-#       end=min(len*i,nrow(NNmatrix))
-#       NNList[[i]]=NNmatrix[start:end,]
-#     }
-#     
-#     if (isTRUE(parallel)){
-#       cl=makeCluster(ncores)
-#       registerDoParallel(cl)
-#       result=foreach(i=1:npartition, .packages=("SpLoc"),.noexport = "SpLocC" )%dopar%{
-#         pU=big.matrix(nrow(NNList[[i]]), nperm, type = "double")
-#         SpLocMeanC(NNList[[i]], ymat, nperm, alpha, seed, pU@address)
-#       }
-#       stopCluster(cl)
-#     } else{
-#       result=list()
-#       for (i in 1:npartition){
-#         pU=big.matrix(nrow(NNList[[i]]), nperm, type = "double")
-#         result[[i]]=SpLocMeanC(NNList[[i]], ymat, nperm, alpha, seed, pU@address)
-#       }
-#     }
-#     
-#     out=combine(result, alpha=alpha)
-#     return(out)
-#     
-#   } else{
-#     pU=big.matrix(nrow(NNmatrix), nperm, type = "double")
-#     out=SpLocMeanC(NNmatrix, ymat, nperm, alpha, seed, pU@address)
-#     out$pvalue=(1+sum(c(out$permMax)>max(out$Tstat,na.rm=TRUE)))/(1+nperm)
-#     out$seed=seed
-#     return(out)    
-#   }
-# }
-# 
-# 
-# SpLocDiff2=function(NNmatrix, ymat, group, nperm=1000, alpha=0.05, seed=NULL, 
-#                    is.sparse=F,partition=F, npartition=1, parallel=F, ncores=1){
-#   if (!all.equal(sort(unique(group)),c(-1,1))){
-#     stop("group should have either 1 or -1.")
-#   }
-#   if (length(group)!=ncol(ymat)){
-#     stop("The number of elements in group does not match with the number of columns in ymat.")
-#   }
-#   if (length(which(is(NNmatrix)=="sparseMatrix"))==0){
-#     stop("NN is not a sparse matrix. Please refer the Matrix R package to convert it.")
-#   }
-#   if ( ncol(NNmatrix)!=nrow(ymat) ){
-#     stop("The number of columns of NN and the number of rows of ymat needs to be the same (# voxels).")
-#   }
-#   if ( alpha<0 |alpha>1){
-#     stop("alpha should range between 0 and 1.")
-#   }
-#   if (is.null(seed)){
-#     stop("Specifying a seed value is required.")
-#   }
-#   if (is.sparse){
-#     index=NULL
-#     for (j in 1:ncol(NNmatrix)){
-#       if(sum(NNmatrix[,j])!=0){ index=c(index,j)}
-#     }
-#     NNmatrix=NNmatrix[,index]
-#     ymat=ymat[index,]
-#   }
-#   if (isTRUE(partition)){
-#     if (is.null(npartition)){
-#       partition=FALSE
-#       print("partition set to be FALSE as npartition is not specified.")
-#     }
-#     else if (npartition==1){
-#       partition=FALSE
-#       print("partition set to be FALSE as npartition is 1.")
-#     }
-#   }
-#   
-#   
-#   if (isTRUE(partition)){
-#     NNList=list()
-#     len=ceiling(nrow(NNmatrix)/npartition)
-#     for (i in 1:npartition){
-#       start=len*(i-1)+1
-#       end=min(len*i,nrow(NNmatrix))
-#       NNList[[i]]=NNmatrix[start:end,]
-#     }
-#     
-#     if (isTRUE(parallel)){
-#       cl=makeCluster(ncores)
-#       registerDoParallel(cl)
-#       result=foreach(i=1:npartition, .packages=("SpLoc"),.noexport = "SpLocC" )%dopar%{
-#         pU=big.matrix(nrow(NNList[[i]]), nperm, type = "double")
-#         SpLocDiffC(NNList[[i]], ymat, group, nperm, alpha, seed, pU@address)
-#       }
-#       stopCluster(cl)
-#     } else{
-#       result=list()
-#       for (i in 1:npartition){
-#         pU=big.matrix(nrow(NNList[[i]]), nperm, type = "double")
-#         result[[i]]=SpLocDiffC(NNList[[i]], ymat, group, nperm, alpha, seed, pU@address)
-#       }
-#     }
-#     
-#     out=combine(result, alpha=alpha)
-#     return(out)
-#     
-#   } else{
-#     pU=big.matrix(nrow(NNmatrix), nperm, type = "double")
-#     out=SpLocDiffC(NNmatrix, ymat, group, nperm, alpha, seed, pU@address)
-#     out$pvalue=(1+sum(c(out$permMax)>max(out$Tstat,na.rm=TRUE)))/(1+nperm)
-#     out$seed=seed
-#     return(out)    
-#   }
-# }
-# 
