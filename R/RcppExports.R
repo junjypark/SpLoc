240afb7971103dd8906e00cb9e82f60fe0255adf
junjypark <73309648+junjypark@users.noreply.github.com>
2020-11-13 22:59:35 -0500

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -17,7 +17,11 @@ SpLocMeanC2 <- function(NNmatrix, ymat, nperm, alpha, s, pU, pY) {
     .Call('_SpLoc_SpLocMeanC2', PACKAGE = 'SpLoc', NNmatrix, ymat, nperm, alpha, s, pU, pY)
 }
 
-SpLocDiffC <- function(NNmatrix, ymat, group, nperm, alpha, s, pU, pY) {
-    .Call('_SpLoc_SpLocDiffC', PACKAGE = 'SpLoc', NNmatrix, ymat, group, nperm, alpha, s, pU, pY)
+SpLocDiffC <- function(NNmatrix, ymat, group, nperm, alpha, s, pU) {
+    .Call('_SpLoc_SpLocDiffC', PACKAGE = 'SpLoc', NNmatrix, ymat, group, nperm, alpha, s, pU)
+}
+
+SpLocDiffC2 <- function(NNmatrix, ymat, group, nperm, alpha, s, pU, pY) {
+    .Call('_SpLoc_SpLocDiffC2', PACKAGE = 'SpLoc', NNmatrix, ymat, group, nperm, alpha, s, pU, pY)
 }
 
