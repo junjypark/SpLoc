3d573fcd03a88aadf631568a940810adbbd6b7eb
junjypark <73309648+junjypark@users.noreply.github.com>
2020-11-13 22:39:54 -0500

    Update SpLoc_support.cpp

--- src/SpLoc_support.cpp
+++ src/SpLoc_support.cpp
@@ -67,8 +67,8 @@ Rcpp::List SpLocMeanC(arma::sp_mat& NNmatrix, arma::mat& ymat, int nperm, double
   U=NNmatrix*ymat*rand;  
 
   set_seed(s);
-  arma::mat<double> rand(n,nperm); rand.randn(); rand=sign(rand);
-  permU=NNmatrix*ymat*rand;
+  arma::mat<double> randmat(n,nperm); randmat.randn(); randmat=sign(randmat);
+  permU=NNmatrix*ymat*randmat;
   
   for (int k=0; k<q; ++k){
     sd=stddev(permU.row(k));
