88a575d1ab72067b8d3bb140a4a181d765f8bf4b
junjypark <73309648+junjypark@users.noreply.github.com>
2021-03-15 11:25:32 -0400

    Update ClusterSearch.R

--- R/ClusterSearch.R
+++ R/ClusterSearch.R
@@ -45,26 +45,56 @@ ClusterSearch=function(Tstat, threshold, NNmatrix){
   return(list(selection=selection, threshold=threshold))
 }
 
-Booster=function(fit, NNmatrix, parallel=F, ncores=1){
-  ind=which(fit$Tstat>fit$thres)
-  Tstatsub=fit$Tstat[ind]
-  NNsub=NNmatrix[ind,]
-  nonzero.index=which(NNsub!=0, arr.ind=T)
-  voxels=sort(unique(nonzero.index[,2]))
+
+ClusterSearch2=function(Tstat, threshold, NNmatrix, fraction=0.1){
   
-  if (parallel){
-    cl=makeCluster(ncores)
-    registerDoParallel(cl)
-    boost=foreach(i=1:length(voxels), .combine="c", .packages="Matrix")%dopar%{
-      max(Tstatsub[which(NNsub[,voxels[i]]!=0)])
+  selection=NULL
+  sig.ind=which(Tstat>threshold)  
+  if (length(sig.ind)>0){
+    Tstat.sub=Tstat[sig.ind]
+    NNmatrix.sub=NNmatrix[sig.ind,,drop=F]
+    
+    if (length(Tstat.sub)==0){bool=F}else{bool=T}
+    while(bool){
+      sig=which(NNmatrix.sub[which(Tstat.sub==max(Tstat.sub, na.rm=T))[1],]!=0)
+      selection=c(selection,sig)
+      num.vec=apply(NNmatrix.sub[,sig,drop=F],1, function(x){sum(x>0)})
+      
+      b=which(NNmatrix.sub>0,arr.ind=T)
+      den.vec=as.numeric(table(b[,1]))
+      
+      out.set=which(num.vec/den.vec>fraction)
+      Tstat.sub=Tstat.sub[-out.set]
+      NNmatrix.sub=NNmatrix.sub[-out.set,,drop=F]
+      if (length(Tstat.sub)==0){bool=F}
     }
-    stopCluster(cl)
-  } else{
-    boost=foreach(i=1:length(voxels), .combine="c",.packages="Matrix")%do%{
-      max(Tstatsub[which(NNsub[,voxels[i]]!=0)])
-    }
+    selection=unique(selection)
   }
   
-  return(list(boost=boost, voxels=voxels))
+  return(list(selection=selection, threshold=threshold))
 }
 
+# 
+# Booster=function(fit, NNmatrix, parallel=F, ncores=1){
+#   ind=which(fit$Tstat>fit$thres)
+#   Tstatsub=fit$Tstat[ind]
+#   NNsub=NNmatrix[ind,]
+#   nonzero.index=which(NNsub!=0, arr.ind=T)
+#   voxels=sort(unique(nonzero.index[,2]))
+#   
+#   if (parallel){
+#     cl=makeCluster(ncores)
+#     registerDoParallel(cl)
+#     boost=foreach(i=1:length(voxels), .combine="c", .packages="Matrix")%dopar%{
+#       max(Tstatsub[which(NNsub[,voxels[i]]!=0)])
+#     }
+#     stopCluster(cl)
+#   } else{
+#     boost=foreach(i=1:length(voxels), .combine="c",.packages="Matrix")%do%{
+#       max(Tstatsub[which(NNsub[,voxels[i]]!=0)])
+#     }
+#   }
+#   
+#   return(list(boost=boost, voxels=voxels))
+# }
+# 
