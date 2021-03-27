6c6ffa09d5943a8f9a90c30828028e9ad8c72420
junjypark <73309648+junjypark@users.noreply.github.com>
2021-03-27 14:59:58 -0400

    Update ClusterSearch.R

--- R/ClusterSearch.R
+++ R/ClusterSearch.R
@@ -1,5 +1,5 @@
 ClusterSearch=function(Tstat, threshold, NNmatrix, alternative=NULL, fraction=0){
-  if (length(Tstat)!=nrow(NNmatrix)){
+  if (length(Tstat)!=nrow(NNmatrix@Dim[1])){
     stop("The number of rows in NNmat needs to be the same as the length of Tstat.")
   }
   if (fraction<0 || fraction>1){
@@ -42,6 +42,91 @@ ClusterSearch=function(Tstat, threshold, NNmatrix, alternative=NULL, fraction=0)
         out.set=which(num.vec/den.vec>fraction)
         TstatList[[th2]]=TstatList[[th2]][-out.set]
         NNmatrixList[[th2]]=NNmatrixList[[th2]][-out.set,,drop=F]
+      }
+      
+      if (length(TstatList[[th]])==0){bool=F}
+    }
+    selectionList[[th]]=c(na.omit(unique(selectionList[[th]])))
+  }
+  
+  return(list(selection=selectionList, threshold=threshold))
+}
+
+ClusterSearch2=function(Tstat, threshold, NNmatrix, alternative=NULL, tau=0, is.eps=TRUE, eps=NULL){
+  if (length(Tstat)!=nrow(NNmatrix@Dim[1])){
+    stop("The number of rows in NNmat needs to be the same as the length of Tstat.")
+  }
+  if (tau<0 || tau>1){
+    stop("tau should be between 0 and 1. A recommended value is 0.2")
+  }
+  if (is.null(alternative)){
+    print("Assuming two-sided alternative. Please change 'alternative' if not.")
+  }
+  if (alternative=="less"){
+    Tstat=-Tstat
+    threshold=-threshold
+  }
+  if (is.eps & is.null(eps)){
+    if (alternative=="two.sided"){
+      eps=1; print("eps set as 1 as a default")      
+    } else {
+      eps=0; print("eps set as 0 as a default")
+    }
+  }
+  
+  n.threshold=length(threshold)
+  if (n.threshold>1){ threshold=sort(threshold, decreasing = T) } 
+  
+  if (is.eps){
+    NNmatEps=NNmatrix[which(Tstat<eps),]
+    np=length(NNmatEps@p)
+    exclude=which(NNmatEps@p[-1]-NNmatEps@p[-np]>0)
+    include=setdiff(1:ncol(NNmatrix), exclude) 
+  }
+  
+  selectionList=list()
+  TstatList=list()
+  NNmatrixList=list()
+  for (th in 1:n.threshold){
+    if (th==1){ sig.ind=which(Tstat>threshold[th]) } 
+    else{ sig.ind=which(Tstat>threshold[th] & Tstat<=threshold[th-1]) }
+    
+    if (is.eps){
+      TstatTemp=Tstat[sig.ind]
+      NNtemp=NNmatrix[sig.ind,,drop=F]
+      NNtemp[,exclude]=0
+      ind=sort(unique(NNtemp@i+1))
+      TstatList[[th]]=Tstat[ind]
+      NNtemp=NNtemp[ind,]
+      NNmatrixList[[th]]=NNtemp
+    } else{
+      TstatList[[th]]=Tstat[sig.ind] 
+      NNmatrixList[[th]]=NNmatrix[sig.ind,,drop=F]
+    }
+  }
+  
+  for (th in 1:n.threshold){
+    if (th==1){ selectionList[[th]]=NA }
+    else{ selectionList[[th]]=selectionList[[th-1]] }
+    
+    if (length(TstatList[[th]])==0){ bool=F } else { bool=T }
+    while(bool){
+      i=which(TstatList[[th]]==max(TstatList[[th]], na.rm=T))[1]
+      sig=which(NNmatrixList[[th]][i,]!=0)
+      if (is.eps){ sig=intersect(sig, include) }
+      
+      selectionList[[th]]=c(selectionList[[th]],sig)
+      print(paste0("search done: ",length(sig)))
+      
+      for (th2 in th:n.threshold){
+        num.vec=apply(NNmatrixList[[th2]][,sig,drop=F],1, function(x){sum(x>0)})
+        den.vec=as.numeric(table(NNmatrixList[[th2]]@i+1))
+        out.set=unique(c(which(num.vec/den.vec>tau),i))
+        
+        if (length(out.set)>0){
+          TstatList[[th2]]=TstatList[[th2]][-out.set]
+          NNmatrixList[[th2]]=NNmatrixList[[th2]][-out.set,,drop=F]
+        }
       }
       
       if (length(TstatList[[th]])==0){bool=F}
