975cbe594e96f21288b570567098e9f213b74ec9
junjypark <73309648+junjypark@users.noreply.github.com>
2021-05-01 15:22:53 -0400

    Update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -31,6 +31,21 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// SpLocMeanC2
+Rcpp::List SpLocMeanC2(arma::mat& ymat, arma::sp_mat& NNmatrix, int nperm, int s, int side);
+RcppExport SEXP _SpLoc_SpLocMeanC2(SEXP ymatSEXP, SEXP NNmatrixSEXP, SEXP npermSEXP, SEXP sSEXP, SEXP sideSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::mat& >::type ymat(ymatSEXP);
+    Rcpp::traits::input_parameter< arma::sp_mat& >::type NNmatrix(NNmatrixSEXP);
+    Rcpp::traits::input_parameter< int >::type nperm(npermSEXP);
+    Rcpp::traits::input_parameter< int >::type s(sSEXP);
+    Rcpp::traits::input_parameter< int >::type side(sideSEXP);
+    rcpp_result_gen = Rcpp::wrap(SpLocMeanC2(ymat, NNmatrix, nperm, s, side));
+    return rcpp_result_gen;
+END_RCPP
+}
 // SpLocDiffC
 Rcpp::List SpLocDiffC(arma::mat& ymat, arma::sp_mat& NNmatrix, arma::vec group, int nperm, int s, int side);
 RcppExport SEXP _SpLoc_SpLocDiffC(SEXP ymatSEXP, SEXP NNmatrixSEXP, SEXP groupSEXP, SEXP npermSEXP, SEXP sSEXP, SEXP sideSEXP) {
@@ -80,6 +95,7 @@ END_RCPP
 static const R_CallMethodDef CallEntries[] = {
     {"_SpLoc_set_seed", (DL_FUNC) &_SpLoc_set_seed, 1},
     {"_SpLoc_SpLocMeanC", (DL_FUNC) &_SpLoc_SpLocMeanC, 5},
+    {"_SpLoc_SpLocMeanC2", (DL_FUNC) &_SpLoc_SpLocMeanC2, 5},
     {"_SpLoc_SpLocDiffC", (DL_FUNC) &_SpLoc_SpLocDiffC, 6},
     {"_SpLoc_MassiveMeanC", (DL_FUNC) &_SpLoc_MassiveMeanC, 4},
     {"_SpLoc_MassiveDiffC", (DL_FUNC) &_SpLoc_MassiveDiffC, 5},
